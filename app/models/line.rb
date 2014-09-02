d1015ddea7ab5ad4e180ce9d4b86efbe3ed1e959
Matt Waechter <student@epicodus.com>
2014-09-02 14:25:38 -0700

    Create a stop class which has many stations and lines through stops

--- app/models/line.rb
+++ app/models/line.rb
@@ -1,4 +1,6 @@
 class Line < ActiveRecord::Base
+  has_many :stops
+  has_many :stations, through: :stops
   validates :name, :presence => true
 end
 
--- spec/models/line_spec.rb
+++ spec/models/line_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
-describe Station do
+describe Line do
   it { should validate_presence_of :name }
+  it { should have_many :stops }
+  it { should have_many(:stations).through(:stops) }
 end
